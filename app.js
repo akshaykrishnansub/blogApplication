beba0855cc047aa201de533e3a7ff991df4f6ded
unknown <akshaykrish533516@gmail.com>
2026-01-02 19:26:36 +0530

    Added changes to app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/",(req,res)=>{
     res.render("home.ejs")
 })
 
+app.get("/about",(req,res)=>{
+    res.render("aboutus.ejs")
+})
+
 app.listen(port, ()=>{
 console.log(`Server running on port: ${port}`)
 })
