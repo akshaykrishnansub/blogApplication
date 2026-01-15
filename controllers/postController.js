ef95f34c09c1442350567def8aef1fe007a6bcf8
unknown <akshaykrish533516@gmail.com>
2026-01-15 09:44:34 +0530

    Updated

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,5 +3,29 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
 const registerUser=async(req,res)=>{
-    
+    //getting data from the input
+    const {first_name,last_name,email,password}=req.body
+    try{
+        //checking if user exists
+        const existingUser=await findUserByEmail(email)
+        if(existingUser)
+            return res.status(400).json({error:'User already registered'})
+
+        //hash the password
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        //insert new user
+        const newUser=await createUser(first_name,last_name,email,hashedPassword)
+
+        //generate a token
+        const token=jwt.sign({id:newUser.id},process.env.JWT_SECRET,{expiresIn:'1h'})
+
+        res.status(201).json({message:'Registration successful',token})
+
+    }catch(err){
+        console.error(err)
+        res.status(500).json({error:'Server error'})
+    }
 }
+
+export {registerUser}
